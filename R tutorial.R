c57d7ea2e5d01ced13f15b01afc87292afd46e89
Patrick Niekamp <pniekamp113@gmail.com>
2024-07-03 13:45:27 -0400

    updated tutorial

--- R tutorial.R
+++ R tutorial.R
@@ -75,3 +75,86 @@ x[x > "a"]
 u <- x > "a"
 u
 
+
+x <- 4
+class(x)
+
+x <- c(4, "a", TRUE)
+class(x)
+
+
+x <- c(1,3, 5) 
+y <- c(3, 2, 10)
+rbind(x,y)
+
+
+x <- list(2, "a", "b", TRUE)
+x[[2]]
+y <- x$a
+y
+
+x <- 1:4
+y <- 2:3
+z <- x+y
+z
+class(z)
+
+
+x <- c(3, 5, 1, 10, 12, 6)
+x[x <= 5] <- 0
+x
+
+getwd()
+
+data <- read.csv("hw1_data.csv")
+data
+
+head(data, n =2)
+
+nrow(data)
+
+tail(data, n=2)
+
+data[, 2]
+
+ozone <- data[,1]
+ozone
+NAs <- is.na(ozone)
+
+sum(NAs)
+
+mean(ozone)
+
+mean_value <- mean(data[["Ozone"]], na.rm = TRUE)
+mean_value
+
+subset_Ozone <- data[data[["Ozone"]] > 31,]
+subset_Ozone
+subset_temp_ozone <- subset_Ozone[subset_Ozone[["Temp"]] > 90, ]
+subset_temp_ozone
+
+mean(subset_temp_ozone[["Solar.R"]], na.rm = TRUE)
+
+data
+
+month6 <- data[data[["Month"]] == 6, ]
+month6
+mean(month6[["Temp"]], na.rm = TRUE)
+
+
+May <- data[data[["Month"]] == 5, ]
+May
+
+max(May[["Ozone"]], na.rm = TRUE)
+
+
+x <- list(2, "a", "b", TRUE)
+x[[1]]
+
+head(data, n = 2)
+
+nrow(data)
+
+tail(data, n = 2)
+
+data[47, ]
